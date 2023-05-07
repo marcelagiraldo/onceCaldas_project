26ead4a86e88bc9d51da047a7f3ceb5623a1db7e
marcelagiraldo <84403422+marcelagiraldo@users.noreply.github.com>
2023-05-07 13:56:17 -0500

    Feature 3 (#2)
    configured path for login and basic login design

---
+++ proy_client/src/pages/SignIn/SignIn.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
+import './SignIn.scss'
+
+const SignIn = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+  return (
+    <Form
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <h2>Inicio de sesión</h2>
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Username!',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="cédula/correo electrónico" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Password!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Contraseña"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default SignIn
