c413555b3a87dd2e97dc4ac10478701c0829b309
yansaid21 <83183760+yansaid21@users.noreply.github.com>
2023-05-07 22:02:20 -0500

    ready login

--- proy_client/src/pages/SignIn/SignIn.js
+++ proy_client/src/pages/SignIn/SignIn.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
+import { Button, Col, Form, Input, Row } from 'antd';
 import './SignIn.scss'
-
+import OnceCaldas from "../../assets/img/png/OnceCaldas.png";
+import UAM from "../../assets/img/png/UAM.png";
+import {FooterPage} from "../../components/FooterPage/FooterPage"
 const SignIn = () => {
   const onFinish = (values) => {
     console.log('Received values of form: ', values);
   };
   return (
+    <div>
+      <Row>
+      <Col xs={24} sm={12} md={8}>
+      <img src={OnceCaldas} alt='Logo' className='Logo'/>
+      </Col>
+      <Col xs={24} sm={12} md={8}>
+      <h2 className='text-out' >Inicio de Sesión</h2>
+      </Col>
+      <Col xs={24} sm={12} md={8}>
+      <img src={UAM} alt='Logo' className='Logo-UAM'/>
+      </Col>
+      </Row>
     <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
+    name="normal_login"
+    className="login-form"
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
     >
-      <h2>Inicio de sesión</h2>
       <Form.Item
         name="username"
         rules={[
@@ -45,10 +58,12 @@ const SignIn = () => {
       </Form.Item>
       <Form.Item>
         <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
+          Ingresar
         </Button>
       </Form.Item>
     </Form>
+    <FooterPage/>
+    </div>
   )
 }
 
